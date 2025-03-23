8f23b761699c9f4dc6872b4b0b79c271d68b7bb5
Kang-Jiho79 <giho2630@gmail.com>
2025-03-23 21:51:01 +0900

    [+] lol directory initial version
    롤디 초기버전

--- LoL diretory/LoL diretory/LoL directory.cpp
+++ LoL diretory/LoL diretory/LoL directory.cpp
@@ -1,5 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <stdio.h>
+#include <string.h>
 using namespace std;
 
 struct lol
@@ -10,108 +11,222 @@ public:
 	int mp;
 	int speed;
 	int range;
-	char position[4];
+	char position[10];
 };
 
-void say(lol camp[], char name[], int& campcount)
+lol camp[100];
+int campcount = 0;
+
+void say(int i)
 {
-	for (int i = 0; i < campcount; i++) {
-		if (camp[i].name == name) {
-			cout << camp[i].name << endl << " 체력 : " << camp[i].hp << endl << " 마나 : " << camp[i].mp << endl << " 속도 : " << camp[i].speed << endl << " 사거리 : " << camp[i].range << endl << " 주 포지션 : " << camp[i].position << endl;
-		}
-	}
+    cout << camp[i].name << endl << "체력 : " << camp[i].hp << endl << "마나 : " << camp[i].mp << endl << "속도 : " << camp[i].speed << endl << "사거리 : " << camp[i].range << endl << "주 포지션 : " << camp[i].position << endl;
 }
-void insert(lol camp[], int& campcount)
+
+void Search(char name[])
+{
+    bool check = true;
+    for (int i = 0; i < campcount; i++) {
+        if (strcmp(camp[i].name, name) == 0) {
+            say(i);
+            check = false;
+        }
+    }
+    if (check)
+        cout << "해당 이름을 가진 챔피언은 없습니다." << endl;
+}
+
+void insert()
 {
 	char name[30];
 	int hp, mp, speed, range;
 	char position[10];
-	cout << "이름(영어, 최대 20자) : ";
-	cin >> name;
-	cout << "체력 : ";
-	cin >> hp;
-	cout << "마나 : ";
-	cin >> mp;
-	cout << "속도 : ";
-	cin >> speed;
-	cout << "사거리 : ";
-	cin >> range;
-	cout << "주포지션(top, jugle, mid, bottom, support) : ";
-	cin >> position;
-	for (int i = 0; i < 20; i++)
-		camp[campcount].name[i] = name[i];
-	camp[campcount].hp = hp;
-	camp[campcount].mp = mp;
-	camp[campcount].speed = speed;
-	camp[campcount].range = range;
-	for (int i = 0; i < 10; i++)
-		camp[campcount].position[i] = position[i];
-	campcount++;
+    while (true) {
+        cout << "이름(영어, 최대 20자) 체력 마나 속도 사거리 주포지션(top, jungle, mid, bottom, support) : ";
+        for (int i = 0; i < 22; i++) {
+            getchar();
+            scanf("%[^\t]s", name);
+            cin >> hp >> mp >> speed >> range >> position;
+            if (strlen(name) <= 20 && hp > 0 && mp >= 0 && speed > 0 && range > 0) {
+                if (strcmp(position, "top") == 0 || strcmp(position, "jungle") == 0 || strcmp(position, "mid") == 0 || strcmp(position, "bottom") == 0 || strcmp(position, "support") == 0) {
+                    for (int j = 0; j < 20; j++)
+                        camp[i].name[j] = name[j];
+                    camp[i].hp = hp;
+                    camp[i].mp = mp;
+                    camp[i].speed = speed;
+                    camp[i].range = range;
+                    for (int j = 0; j < 10; j++)
+                        camp[i].position[j] = position[j];
+                    campcount++;
+                    cout << camp[i].position << endl;
+                    cout << "추가되었습니다." << endl;
+                }
+                else
+                    cout << "잘못입력하셨습니다 다시 입력해주십시오" << endl;
+            }
+            else
+                cout << "잘못입력하셨습니다 다시 입력해주십시오" << endl;
+        }
+        return;
+    }
 }
-void deletename(lol camp[], int& campcount, char name[])
+
+void deletename(char name[])
 {
 	for (int i = 0; i < campcount; i++) {
-		if (camp[i].name == name) {
+		if (strcmp(camp[i].name, name) == 0) {
 			for (int j = i; j < campcount; j++) {
 				camp[j] = camp[j + 1];
 			}
+            campcount--;
+            cout << "챔피언이 삭제되었습니다." << endl;
+            return;
 		}
 	}
-	campcount--;
+    cout << "해당 챔피언이 없습니다." << endl;
 }
 
-void deleteall(lol camp[], int& campcount, char position[])
+void deleteall(char position[])
 {
 	for (int i = 0; i < campcount; i++) {
-		if (camp[i].position == position) {
+		if (strcmp(camp[i].position,position) == 0) {
 			for (int j = i; j < campcount; j++)
 				camp[j] = camp[j + 1];
 			i--;
 			campcount--;
 		}
 	}
+    cout << "해당 포지션의 챔피언이 삭제되었습니다" << endl;
+}
+
+void Findmaxhp()
+{
+    int maxhp = 0;
+    for (int i = 0; i < campcount; i++) {
+        if (camp[i].hp > maxhp)
+            maxhp = camp[i].hp;
+    }
+    for (int i = 0; i < campcount; i++) {
+        if (camp[i].hp == maxhp)
+            say(i);
+    }
+}
+
+void sortbyhp()
+{
+    for (int i = 0; i < campcount - 1; i++) {
+        for (int j = i + 1; j < campcount; j++) {
+            if (camp[i].hp < camp[j].hp) {
+                lol temp = camp[i];
+                camp[i] = camp[j];
+                camp[j] = temp;
+            }
+        }
+    }
+    cout << "체력 기준으로 정렬완료" << endl;
 }
 
 int main()
 {
-	lol camp[100];
-	char cho[10];
-	char name[30];
-	char position[10];
-	int campcount = 0;
+	int cho;
+    int last;
 	while (true)
 	{
-		cout << "========================================" << endl;
-		cout << "Search [챔피언이름] : 주어진 챔피언의 정보를 배열에서 찾아 출력한다." << endl << "Insert : 새로운 챔피언의 정보를 입력받아 배열에 삽입한다." << endl << "Delete [챔피언이름] : 주어진 챔피언에 대한 정보를 배열에서 삭제한다." << endl;
-		cout << "DeleteAll [position](top, jugle, mid, bottom, support) : 주어진 위치의 모든 챔피언 정보를 배열에서 삭제한다." << endl << "PrintAll : 모든 챔피언의 정보를 배열에 저장된 순서대로 출력한다.(정렬을 수행한 후 PrintAll을 수행하면 정렬된 순서대로 출력되어야 합니다.)" << endl;
-		cout << "FindMaxHp: 가장 체력이 큰 챔피언의 정보를 출력한다.(HP가 가장 큰 챔피언이 여럿이면 모두 출력한다)" << endl << "SortByHp: 체력이 큰 챔피언부터 순서대로 저장한다.(HP가 동일한 챔피언은 연속으로 저장되어야 합니다.)" << endl;
+        cout << "========================================" << endl << "명령어는 앞의 숫자로 입력한다." << endl;
+        cout << "1. Search  2. Insert   3. Delete   4. DeleteAll" << endl << "5. PrintAll   6. FindMaxHp  7. SortByHp" << endl;
 		cout << "========================================" << endl;
 		cin >> cho;
-		if (cho == "Search") {
-			cin >> name;
-			say(camp, name, campcount);
-		}
-		else if (cho == "Insert") {
-			insert(camp, campcount);
-		}
-		else if (cho == "Delete") {
-			cin >> name;
-			deletename(camp, campcount, name);
-		}
-		else if (cho == "DeleteAll") {
-
-		}
-		else if (cho == "PrintAll") {
-
-		}
-		else if (cho == "FindMaxHp") {
-
-		}
-		else if (cho == "SortByHp") {
-
-		}
-		else {
-
-		}
+        switch (cho)
+        {
+        case 1: 
+        {
+            char name[30];
+            while (true) {
+                cout << "이름(영어, 최대 20자) : ";
+                getchar();
+                scanf("%[^\n]s", name);
+                if (strlen(name) <= 20) {
+                    Search(name);
+                    break;
+                }
+                else {
+                    cout << "이름이 너무 긺" << endl;
+                }
+            }
+            break;
+        }
+        case 2:
+        {
+            insert();
+            break;
+        }
+        case 3:
+        {
+            char name[30];
+            while (true) {
+                cout << "이름(영어, 최대 20자) : ";
+                getchar();
+                scanf("%[^\n]s", name);
+                if (strlen(name) <= 20) {
+                    deletename(name);
+                    break;
+                }
+                else {
+                    cout << "이름이 너무 긺" << endl;
+                }
+            }
+            break;
+        }
+        case 4:
+        {
+            char position[10];
+            while (true) {
+                cout << "주포지션(top, jungle, mid, bottom, support) : ";
+                cin >> position;
+                if (strcmp(position, "top") == 0 || strcmp(position, "jungle") == 0 || strcmp(position, "mid") == 0 || strcmp(position, "bottom") == 0 || strcmp(position, "support") == 0) {
+                    deleteall(position);
+                    break;
+                }
+                else {
+                    cout << "포지션을 잘못 입력하였습니다." << endl;
+                }
+            }
+            break;
+        }
+        case 5:
+        {
+            for (int i = 0; i < campcount; i++)
+                say(i);
+            break;
+        }
+        case 6:
+        {
+            Findmaxhp();
+            break;
+        }
+        case 7:
+        {
+            sortbyhp();
+            break;
+        }
+        default:
+        {
+            cout << "잘못 입력하였습니다." << endl;
+            break;
+        }
+        }
+        while (true) {
+            cout << "계속 진행하겠다면 1 그만하겠다면 2 : " << endl;
+            cin >> last;
+            if (last == 1) {
+                system("cls");
+                break;
+            }
+            else if (last == 2) {
+                return 0;
+            }
+            else {
+                cout << "다시 입략하십시오" << endl;
+            }
+        }
 	}
 }
