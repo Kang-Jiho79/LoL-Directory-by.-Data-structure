a24988a81482887ecadedb81788f1e6009bde9e9
Kang-Jiho79 <giho2630@gmail.com>
2025-04-06 23:43:27 +0900

    [+] 25.04.06 {1}
    재귀함수로 변경

--- LoL diretory/LoL diretory/LoL directory.cpp
+++ LoL diretory/LoL diretory/LoL directory.cpp
@@ -3,217 +3,259 @@
 #include <string.h>
 using namespace std;
 
-struct lol
-{
+struct lol {
 public:
-	char name[30];
-	int hp;
-	int mp;
-	int speed;
-	int range;
-	char position[10];
+    char name[30] = "\0";
+    int hp = 0;
+    int mp = 0;
+    int speed = 0;
+    int range = 0;
+    char position[10] = "\0";
 };
 
-lol camp[100];
-int campcount = 0;
+lol champ[100];
+int champcount = 0;
 
-void say(int i)
-{
-    cout << camp[i].name << endl << "체력 : " << camp[i].hp << endl << "마나 : " << camp[i].mp << endl << "속도 : " << camp[i].speed << endl << "사거리 : " << camp[i].range << endl << "주 포지션 : " << camp[i].position << endl;
+void say(int i) {
+    cout << champ[i].name << endl
+        << "체력 : " << champ[i].hp << endl
+        << "마나 : " << champ[i].mp << endl
+        << "속도 : " << champ[i].speed << endl
+        << "사거리 : " << champ[i].range << endl
+        << "주 포지션 : " << champ[i].position << endl;
 }
 
-void Search(char name[])
-{
-    bool check = true;
-    for (int i = 0; i < campcount; i++) {
-        if (strcmp(camp[i].name, name) == 0) {
-            say(i);
-            check = false;
+void SearchRecursive(int i, char name[], bool found = false) {
+    if (i >= champcount) {
+        if (!found)
+            cout << "해당 이름을 가진 챔피언은 없습니다." << endl;
+        return;
+    }
+    if (strcmp(champ[i].name, name) == 0) {
+        say(i);
+        found = true;
+    }
+    SearchRecursive(i + 1, name, found);
+}
+
+void insert() {
+    char name[30];
+    int hp, mp, speed, range;
+    char position[10];
+
+    cout << "이름(영어, 최대 20자) 체력 마나 속도 사거리 주포지션(top, jungle, mid, bottom, support) : ";
+    cin.ignore();
+    scanf("%[^	]s", name);
+    if (!(cin >> hp >> mp >> speed >> range >> position)) {
+        cout << "입력 오류입니다." << endl;
+        cin.clear(); cin.ignore(1000, '\n');
+        return;
+    }
+
+    if (strlen(name) <= 20 && hp > 0 && mp >= 0 && speed > 0 && range > 0) {
+        if (strcmp(position, "top") == 0 || strcmp(position, "jungle") == 0 || strcmp(position, "mid") == 0 || strcmp(position, "bottom") == 0 || strcmp(position, "support") == 0) {
+            for (int i = 0; i < 100; i++) {
+                if (champ[i].name[0] == '\0') {
+                    strncpy(champ[i].name, name, sizeof(champ[i].name));
+                    champ[i].hp = hp;
+                    champ[i].mp = mp;
+                    champ[i].speed = speed;
+                    champ[i].range = range;
+                    strncpy(champ[i].position, position, sizeof(champ[i].position));
+                    champcount++;
+                    cout << champ[i].position << endl;
+                    cout << "추가되었습니다." << endl;
+                    return;
+                }
+            }
+            cout << "저장 공간이 없습니다." << endl;
+        }
+        else {
+            cout << "잘못입력하셨습니다." << endl;
         }
     }
-    if (check)
-        cout << "해당 이름을 가진 챔피언은 없습니다." << endl;
+    else {
+        cout << "잘못입력하셨습니다." << endl;
+    }
 }
 
-void insert()
-{
-	char name[30];
-	int hp, mp, speed, range;
-	char position[10];
-    while (true) {
-        cout << "이름(영어, 최대 20자) 체력 마나 속도 사거리 주포지션(top, jungle, mid, bottom, support) : ";
-        for (int i = 0; i < 22; i++) {
-            getchar();
-            scanf("%[^\t]s", name);
-            cin >> hp >> mp >> speed >> range >> position;
-            if (strlen(name) <= 20 && hp > 0 && mp >= 0 && speed > 0 && range > 0) {
-                if (strcmp(position, "top") == 0 || strcmp(position, "jungle") == 0 || strcmp(position, "mid") == 0 || strcmp(position, "bottom") == 0 || strcmp(position, "support") == 0) {
-                    for (int j = 0; j < 20; j++)
-                        camp[i].name[j] = name[j];
-                    camp[i].hp = hp;
-                    camp[i].mp = mp;
-                    camp[i].speed = speed;
-                    camp[i].range = range;
-                    for (int j = 0; j < 10; j++)
-                        camp[i].position[j] = position[j];
-                    campcount++;
-                    cout << camp[i].position << endl;
-                    cout << "추가되었습니다." << endl;
-                }
-                else
-                    cout << "잘못입력하셨습니다 다시 입력해주십시오" << endl;
-            }
-            else
-                cout << "잘못입력하셨습니다 다시 입력해주십시오" << endl;
+void deletenameRecursive(int i, char name[]) {
+    if (i >= champcount) {
+        cout << "해당 챔피언이 없습니다." << endl;
+        return;
+    }
+    if (strcmp(champ[i].name, name) == 0) {
+        champ[i].name[0] = '\0';
+        champ[i].hp = champ[i].mp = champ[i].speed = champ[i].range = 0;
+        champ[i].position[0] = '\0';
+        champcount--;
+        cout << "챔피언이 삭제되었습니다." << endl;
+        return;
+    }
+    deletenameRecursive(i + 1, name);
+}
+
+void deleteallRecursive(int i, char position[]) {
+    if (i >= champcount) {
+        cout << "해당 포지션의 챔피언이 삭제되었습니다" << endl;
+        return;
+    }
+    if (strcmp(champ[i].position, position) == 0) {
+        champ[i].name[0] = '\0';
+        champ[i].hp = champ[i].mp = champ[i].speed = champ[i].range = 0;
+        champ[i].position[0] = '\0';
+        champcount--;
+    }
+    deleteallRecursive(i + 1, position);
+}
+
+void FindmaxhpRecursive(int i = 0, int maxhp = 0) {
+    if (i >= champcount) {
+        for (int j = 0; j < champcount; j++) {
+            if (champ[j].hp == maxhp)
+                say(j);
         }
         return;
     }
+    if (champ[i].hp > maxhp)
+        maxhp = champ[i].hp;
+    FindmaxhpRecursive(i + 1, maxhp);
 }
 
-void deletename(char name[])
-{
-	for (int i = 0; i < campcount; i++) {
-		if (strcmp(camp[i].name, name) == 0) {
-			for (int j = i; j < campcount; j++) {
-				camp[j] = camp[j + 1];
-			}
-            campcount--;
-            cout << "챔피언이 삭제되었습니다." << endl;
-            return;
-		}
-	}
-    cout << "해당 챔피언이 없습니다." << endl;
-}
-
-void deleteall(char position[])
-{
-	for (int i = 0; i < campcount; i++) {
-		if (strcmp(camp[i].position,position) == 0) {
-			for (int j = i; j < campcount; j++)
-				camp[j] = camp[j + 1];
-			i--;
-			campcount--;
-		}
-	}
-    cout << "해당 포지션의 챔피언이 삭제되었습니다" << endl;
-}
-
-void Findmaxhp()
-{
-    int maxhp = 0;
-    for (int i = 0; i < campcount; i++) {
-        if (camp[i].hp > maxhp)
-            maxhp = camp[i].hp;
+void sortbyhpRecursive(int i = 0) {
+    if (i >= champcount - 1) {
+        cout << "체력 기준으로 정렬완료" << endl;
+        return;
     }
-    for (int i = 0; i < campcount; i++) {
-        if (camp[i].hp == maxhp)
-            say(i);
-    }
-}
-
-void sortbyhp()
-{
-    for (int i = 0; i < campcount - 1; i++) {
-        for (int j = i + 1; j < campcount; j++) {
-            if (camp[i].hp < camp[j].hp) {
-                lol temp = camp[i];
-                camp[i] = camp[j];
-                camp[j] = temp;
-            }
+    for (int j = i + 1; j < champcount; j++) {
+        if (champ[i].hp < champ[j].hp) {
+            lol temp = champ[i];
+            champ[i] = champ[j];
+            champ[j] = temp;
         }
     }
-    cout << "체력 기준으로 정렬완료" << endl;
+    sortbyhpRecursive(i + 1);
 }
 
-int main()
-{
-	int cho;
-    int last;
-	while (true)
-	{
+void printAllRecursive(int i = 0) {
+    if (i >= champcount) return;
+    say(i);
+    printAllRecursive(i + 1);
+}
+
+void initalsetting_recursive(int i = 0) {
+    if (i >= 22) return;
+
+    char name[30];
+    int hp, mp, speed, range;
+    char position[10];
+
+    if (i == 0)
+        cout << "초기 세팅입니다. text 파일 전체를 복사해서 붙여넣어 주세요." << endl;
+
+    if (i != 0)
+        getchar();
+
+    if (scanf("%[^\t]s", name) != 1) {
+        cout << "이름 입력 오류, 다시 시도해 주세요." << endl;
+        cin.clear(); cin.ignore(1000, '\n');
+        initalsetting_recursive(i);
+        return;
+    }
+
+    if (!(cin >> hp >> mp >> speed >> range >> position)) {
+        cout << "숫자 입력 오류, 다시 시도해 주세요." << endl;
+        cin.clear(); cin.ignore(1000, '\n');
+        initalsetting_recursive(i);
+        return;
+    }
+
+    if (strlen(name) <= 20 && hp > 0 && mp >= 0 && speed > 0 && range > 0) {
+        if (strcmp(position, "top") == 0 || strcmp(position, "jungle") == 0 || strcmp(position, "mid") == 0 || strcmp(position, "bottom") == 0 || strcmp(position, "support") == 0) {
+            strncpy(champ[i].name, name, sizeof(champ[i].name));
+            champ[i].hp = hp;
+            champ[i].mp = mp;
+            champ[i].speed = speed;
+            champ[i].range = range;
+            strncpy(champ[i].position, position, sizeof(champ[i].position));
+            champcount++;
+            cout << "추가되었습니다." << endl;
+        }
+        else {
+            cout << "잘못입력하셨습니다. (포지션 오류)" << endl;
+        }
+    }
+    else {
+        cout << "잘못입력하셨습니다. (조건 불충족)" << endl;
+    }
+
+    initalsetting_recursive(i + 1);
+}
+
+int main() {
+    int cho, last;
+    initalsetting_recursive();
+    while (true) {
         cout << "========================================" << endl << "명령어는 앞의 숫자로 입력한다." << endl;
         cout << "1. Search  2. Insert   3. Delete   4. DeleteAll" << endl << "5. PrintAll   6. FindMaxHp  7. SortByHp" << endl;
-		cout << "========================================" << endl;
-		cin >> cho;
-        switch (cho)
-        {
-        case 1: 
-        {
+        cout << "========================================" << endl;
+        cin >> cho;
+        switch (cho) {
+        case 1: {
             char name[30];
-            while (true) {
-                cout << "이름(영어, 최대 20자) : ";
-                getchar();
-                scanf("%[^\n]s", name);
-                if (strlen(name) <= 20) {
-                    Search(name);
-                    break;
-                }
-                else {
-                    cout << "이름이 너무 긺" << endl;
-                }
+            cout << "이름(영어, 최대 20자) : ";
+            cin.ignore();
+            scanf("%[^\n]s", name);
+            if (strlen(name) <= 20) {
+                SearchRecursive(0, name);
+            }
+            else {
+                cout << "이름이 너무 긺" << endl;
             }
             break;
         }
         case 2:
-        {
             insert();
             break;
-        }
-        case 3:
-        {
+        case 3: {
             char name[30];
-            while (true) {
-                cout << "이름(영어, 최대 20자) : ";
-                getchar();
-                scanf("%[^\n]s", name);
-                if (strlen(name) <= 20) {
-                    deletename(name);
-                    break;
-                }
-                else {
-                    cout << "이름이 너무 긺" << endl;
-                }
+            cout << "이름(영어, 최대 20자) : ";
+            cin.ignore();
+            scanf("%[^\n]s", name);
+            if (strlen(name) <= 20) {
+                deletenameRecursive(0, name);
+            }
+            else {
+                cout << "이름이 너무 긺" << endl;
             }
             break;
         }
-        case 4:
-        {
+        case 4: {
             char position[10];
-            while (true) {
-                cout << "주포지션(top, jungle, mid, bottom, support) : ";
-                cin >> position;
-                if (strcmp(position, "top") == 0 || strcmp(position, "jungle") == 0 || strcmp(position, "mid") == 0 || strcmp(position, "bottom") == 0 || strcmp(position, "support") == 0) {
-                    deleteall(position);
-                    break;
-                }
-                else {
-                    cout << "포지션을 잘못 입력하였습니다." << endl;
-                }
+            cout << "주포지션(top, jungle, mid, bottom, support) : ";
+            cin >> position;
+            if (strcmp(position, "top") == 0 || strcmp(position, "jungle") == 0 || strcmp(position, "mid") == 0 || strcmp(position, "bottom") == 0 || strcmp(position, "support") == 0) {
+                deleteallRecursive(0, position);
+            }
+            else {
+                cout << "포지션을 잘못 입력하였습니다." << endl;
             }
             break;
         }
         case 5:
-        {
-            for (int i = 0; i < campcount; i++)
-                say(i);
+            printAllRecursive();
             break;
-        }
         case 6:
-        {
-            Findmaxhp();
+            FindmaxhpRecursive();
             break;
-        }
         case 7:
-        {
-            sortbyhp();
+            sortbyhpRecursive();
             break;
-        }
         default:
-        {
             cout << "잘못 입력하였습니다." << endl;
             break;
         }
-        }
+
         while (true) {
             cout << "계속 진행하겠다면 1 그만하겠다면 2 : " << endl;
             cin >> last;
@@ -228,5 +270,5 @@ int main()
                 cout << "다시 입략하십시오" << endl;
             }
         }
-	}
+    }
 }
